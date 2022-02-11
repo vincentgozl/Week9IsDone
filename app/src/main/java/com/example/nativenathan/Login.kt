06621fef43174e7c9dbf487e46ecbc811dfe28ec
Vincent <vincentgozali21@gmail.com>
2022-02-11 19:58:50 +0700

    Vincent1

--- app/src/main/java/com/example/nativenathan/Login.kt
+++ app/src/main/java/com/example/nativenathan/Login.kt
@@ -5,6 +5,5 @@ import android.widget.Toast
 class Login (var username:String, var password:String) {
     override fun toString(): String {
         return "$username $password"
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/nativenathan/MainActivity.kt
+++ app/src/main/java/com/example/nativenathan/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nativenathan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     // Perlu bikin fitur login
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Nathan Hendry", Toast.LENGTH_SHORT).show()
     }
 }
