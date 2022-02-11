63f5968f72cf66c35b9bf143dac85b0686b8af62
ACER <s160419034@student.ubaya.ac.id>
2022-02-11 19:48:28 +0700

    test 2

--- app/src/main/java/com/example/nativenathan/Login.kt
+++ app/src/main/java/com/example/nativenathan/Login.kt
@@ -1,7 +1,10 @@
 package com.example.nativenathan
 
+import android.widget.Toast
+
 class Login (var username:String, var password:String) {
     override fun toString(): String {
         return "$username $password"
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
     }
 }
