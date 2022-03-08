f5ffae2f5cc7e0ffea96d23fbf91922fff953a6d
Vincent <vincentgozali21@gmail.com>
2022-03-08 19:53:18 +0700

    Week 4

--- app/src/main/java/com/example/nativenathan/Login.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nativenathan
-
-import android.widget.Toast
-
-class Login (var username:String, var password:String) {
-    override fun toString(): String {
-        return "$username $password"
-    }
-}
